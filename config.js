91b24a5a5caf4f81f141dd611cd84830a4117a14
Azagaria <116659338+Azagarialol@users.noreply.github.com>
2022-10-27 19:20:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,19 @@
 module.exports = {
-  prefix: "prefix you'll use", // Your Bot Prefix
+  // BOT SETTINGS
+  prefix: "",
+  client_id: "1234567890",
+  owners: ["1234567890", "1234567890"],
+  // TOKEN IN .ENV
+  // CLIENT_SECRET IN .ENV
+  // MONGODB IN .ENV
 
-  client_id: "1234567890", // Your Application Client ID
-  redirect_uri: "https://YOURPACKAGE.YOURNAMEWILLBEHERE.repl.co/authed", // Your Application Redirect Url
-  oauth_link: "", // NEEDED!!!
-  owners: ["676846295633231903"] // WL Ids (If you don't have whitelisted id you can't use cmds)
 
+  // AUTH SETTINGS
+  redirect_uri: "https://YOURREPHERE.YOURNAMEHERE.repl.co/authed", 
+  oauth_link: "https://discord.com/api/oauth2/authorize?client_id=1035190292854349844&redirect_uri=https%3A%2F%YOURREPHERE.YOURNAMEHERE.repl.co%2Fauthed&response_type=code&scope=email%20guilds%20guilds.join",
+
+  // WEBHOOK SETTINGS (DETECTS NEW AUTHS)
+  webhook_URL: "Webhook URL",
+  webhook_NAME: "Webhook Name",
+  webhook_AVATAR: "Webhook Avatar (PFP)"
 }
